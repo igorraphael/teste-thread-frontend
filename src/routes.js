4390988058ed2d7320f50303d596be4e0f4996de
Igor Raphael <igorraphaeel@live.com>
2020-03-19 00:28:41 -0300

    add router dom, simples routes

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from "./pages/Main";
+import New from "./pages/New";
+
+export default function Routes(){
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/new" component={New} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
